cccbe78ff5a32d1f44a2d3dcf53c31110000882b
zhufengzhufeng <1035465284@qq.com>
2017-08-30 15:48:30 +0800

    基本结构

--- secondday/todo.js
+++ secondday/todo.js
@@ -1,9 +1,26 @@
 let vm = new Vue({
     el:'#app',
     data:{
+        todo:{isSelected:false,title:''},
         todos:[
             {title:'回家洗碗',isSelected:false},
             {title:'给媳妇买包',isSelected:false}
-        ]
+        ],
+    },
+    methods:{
+        addTodo(){
+            //将当前输入的内容 放入到数组中
+            this.todos.push(this.todo);
+            //将输入框中的内容删除
+            this.todo = {isSelected:false,title:''};
+        },
+        remove(todo){
+            this.todos = this.todos.filter(item=>item!==todo);
+        }
+    },
+    computed:{
+        count(){
+            return this.todos.filter(item=>!item.isSelected).length;
+        }
     }
 });
