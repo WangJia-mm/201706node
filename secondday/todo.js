dc0f3c6c71e0beaa7237aa066b40fff932ce99bb
zhufengzhufeng <1035465284@qq.com>
2017-08-30 16:12:56 +0800

    取消

--- secondday/todo.js
+++ secondday/todo.js
@@ -6,6 +6,7 @@ let vm = new Vue({
             {title:'回家洗碗',isSelected:false},
             {title:'给媳妇买包',isSelected:false}
         ],
+        current:''
     },
     methods:{
         addTodo(){
@@ -16,10 +17,19 @@ let vm = new Vue({
         },
         remove(todo){
             this.todos = this.todos.filter(item=>item!==todo);
+        },
+        save(){
+            this.current = ''; //将当前点击的 和任何一项都不相等则会变回默认状态
+        },
+        change(todo){ //当双击时将当前的元素保存到current变量上
+            this.current = todo;
+        },
+        cancel(){ //当用户点击esc时 要做的是将当前项的值改回原有状态
+
         }
     },
     computed:{
-        count(){
+        count(){ //计算未完成属性的个数
             return this.todos.filter(item=>!item.isSelected).length;
         }
     }
